3a0914f434d12b05b8d4686ea8efb48c14fd5768
Leh-zealand-2026 <leh001@edu.zealand.dk>
2026-03-23 21:47:21 +0100

    started on defining classes

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
  #Obligatorisk opgave 1 - Library Management System
+
+# We want to create a simple library management system.
+
+# 1. First we define the classes, these are blueprints that define our data structure
+
+class Book:
+    def __init__(self, book_id, title, author, copies):
+        self.book_id = book_id
+        self.title = title
+        self.author = author
+        self.copies = copies
+        
+
+class Member:
+    def __init__(self, member_id, name, borrowed_books):
+        self.member_id = member_id
+        self.name = name
+        self.borrowed_books = borrowed_books
+        self.return_book = self.return_book
+
+class Library:
+    def __init__(self, books, members):
+        self.books = books
+        self.members = members
+        
