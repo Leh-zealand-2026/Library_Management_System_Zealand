8c8271e7b7a7872b382f0c31b589d773819c3aad
Leh-zealand-2026 <leh001@edu.zealand.dk>
2026-03-24 03:59:27 +0100

    added method inside book class

--- main.py
+++ main.py
@@ -4,14 +4,21 @@
 
 # 1. First we define the classes, these are blueprints that define our data structure
 
+# self allows us to establish a variable in one function and use it in another
+
 class Book:
     def __init__(self, book_id, title, author, copies):
         self.book_id = book_id
         self.title = title
         self.author = author
         self.copies = copies
+    # Defining method, these are functions that are tied to an object
+    def display_info(self):
+        print(f"ID: {self.book_id}")
+        print(f"Title: {self.title}")  
+        print(f"Author: {self.author}")  
+        print(f"Copies: {self.copies}")   
         
-
 class Member:
     def __init__(self, member_id, name, borrowed_books):
         self.member_id = member_id
