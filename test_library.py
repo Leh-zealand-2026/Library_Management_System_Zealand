fca460c0d5f1635a80afd0f5362164f5379f8141
Leh-zealand-2026 <leh001@edu.zealand.dk>
2026-03-27 14:38:35 +0100

    tested add_book in library and it passed

--- test_library.py
+++ test_library.py
@@ -3,11 +3,19 @@
 # Pytest checks for any files named test to open and inside those files
 # it checks for any functions named test to test, so thats why we need to begin with test_
 
-# We expect this to pass
-def test_pass():
-    assert 1 + 1 == 2
 
-# We expect this to fail
-def test_fail():
-    assert 2 + 2 == 5
+# We create a test_ function for every function that we have created in our main file
 
+from main import Book, Member, Library
+
+# Testing our add book function in library()
+
+def test_add_book():
+    lib = Library()
+    # Set the values of our book object
+    book = Book("2026", "Harry Potter", "JK Rowling", 10)
+
+    lib.add_book(book)
+
+    # Check if the book ID 2026 matches any book in our database
+    assert "2026" in lib.books
