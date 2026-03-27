5bddfb60fdd786e3e08362cb9f728ff7cdafcf2e
Leh-zealand-2026 <leh001@edu.zealand.dk>
2026-03-27 14:27:00 +0100

    created tests to see if pytest pass and fails as expected

--- test_library.py
+++ test_library.py
@@ -1,4 +1,13 @@
 # Pytest file 
 
+# Pytest checks for any files named test to open and inside those files
+# it checks for any functions named test to test, so thats why we need to begin with test_
+
+# We expect this to pass
 def test_pass():
     assert 1 + 1 == 2
+
+# We expect this to fail
+def test_fail():
+    assert 2 + 2 == 5
+
