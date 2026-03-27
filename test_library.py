d8601c08db7cf570a686563c98760e380a235b8c
Leh-zealand-2026 <leh001@edu.zealand.dk>
2026-03-27 14:43:55 +0100

    add member test failed pytest

--- test_library.py
+++ test_library.py
@@ -19,3 +19,15 @@ def test_add_book():
 
     # Check if the book ID 2026 matches any book in our database
     assert "2026" in lib.books
+
+# Test for adding members
+
+def test_add_member():
+    lib = Library()
+    # Set values of our member
+    member = Member("23", "Lebron James")
+
+    lib.add_member(member)
+
+    # Check if any member id matches who we just added
+    assert "23" in lib.members
